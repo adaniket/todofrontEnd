f4be5d42d644a2766705dd43a793ee247a434f33
Aniket <aniketad07@gmail.com>
2024-10-02 16:04:20 +0530

    loader added in routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { Suspense, useState } from "react";
 import "./App.css";
 import { RouterProvider, useLocation } from "react-router-dom";
 import routes from "./routes/routes";
-
+import BarLoader from "./components/Loader/BarLoader";
 
 function App() {
   return (
-    <Suspense fallback={<h1>Loading</h1>}>
+    <Suspense
+      fallback={
+        <div className="h-[80vh] flex items-center justify-center">
+          <BarLoader />
+        </div>
+      }
+    >
       <RouterProvider router={routes} />
     </Suspense>
   );
